6969bd2ee89fb15d92b101fcfc87ca5207fb459e
ayushb33 <ayushbehera777@gmail.com>
2026-05-05 12:07:12 +0530

    Update reviewModel.js

--- backend/src/models/reviewModel.js
+++ backend/src/models/reviewModel.js
@@ -1,6 +1,11 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const Review = {
   // Submit new review
