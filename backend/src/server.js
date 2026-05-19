6aca7128a57b06b33ac2800361b3a1e7ed09aba1
ayushb33 <166656089+ayushb33@users.noreply.github.com>
2026-05-19 20:10:57 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,6 +30,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
