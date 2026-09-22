7926d921b431fc377bff0030ae94174f49ded328
ayushb33 <166656089+ayushb33@users.noreply.github.com>
2026-09-22 14:36:25 +0530

    qb

--- ngo-web/src/pages/QuestionBank.jsx
+++ ngo-web/src/pages/QuestionBank.jsx
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const ACCESS_CODE = process.env.REACT_APP_QUESTION_ACCESS_CODE; // 
-const PDF_FILE = "/question-banks/paper 2 for 7-9th.pdf"; // 
+const PDF_FILE = "/question-banks/SNEQC_26-27 Question Bank.pdf"; // 
 
 // ✅ Responsive PDF viewer
 const PdfJsViewer = ({ src }) => {
